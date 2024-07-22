3e476af51137f04e4f96b2614889c997eb449094
Yan Bussieres <yan.buss22@gmail.com>
2024-07-22 10:00:36 -0400

    move .wezterm.lua

---
+++ dot_wezterm.lua
@@ -0,0 +1,24 @@
+local wezterm = require("wezterm")
+local config = {}
+config.font = wezterm.font("Hack Nerd Font")
+
+config.color_scheme = "Tokyo Night"
+config.font_size = 16.0
+
+config.window_close_confirmation = "NeverPrompt"
+
+local act = wezterm.action
+config.keys = {
+	{
+		key = "d",
+		mods = "CMD",
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "D",
+		mods = "SHIFT|CMD",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+}
+
+return config
