018be3c76295aa359cf446bc5bd67499440980cf
Yan Bussieres <yan.buss22@gmail.com>
2024-08-06 09:01:43 -0400

    add mininimal oh-m-posh setup

---
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,48 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    -- refer to `:h file-pattern` for more examples
+    "BufReadPre path/to/my-vault/*.md",
+    "BufNewFile path/to/my-vault/*.md",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    -- see below for full list of options 👇
+    workspaces = {
+      {
+        name = "dev",
+        path = "~/obsidian/rock",
+      },
+      {
+        name = "no-vault",
+        path = function()
+          -- alternatively use the CWD:
+          -- return assert(vim.fn.getcwd())
+          return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+        end,
+        overrides = {
+          notes_subdir = vim.NIL, -- have to use 'vim.NIL' instead of 'nil'
+          new_notes_location = "current_dir",
+          templates = {
+            folder = vim.NIL,
+          },
+          disable_frontmatter = true,
+        },
+      },
+    },
+
+    templates = {
+      folder = "~/obsidian/rock/templates",
+      date_format = "%Y-%m-%d-%a",
+      time_format = "%H:%M",
+    },
+  },
+}
