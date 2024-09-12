92e9adbd986c4cd4deac4a3f598e66812d1de980
Yan Bussieres <yan.buss22@gmail.com>
2024-09-12 01:16:52 -0400

    cleanup

--- dot_config/sketchybar/plugins/executable_aerospace.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on \
-                 label.align=center\ 
-else
-    sketchybar --set $NAME background.drawing=off
-fi
