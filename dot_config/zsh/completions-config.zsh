519a6d363d435f5f4560d4f6ea446f9dbcbf51f4
Yan Bussieres <yan.buss22@gmail.com>
2025-03-05 09:30:24 -0500

    back to stow

--- dot_config/zsh/completions-config.zsh
+++
@@ -1,6 +0,0 @@
-zstyle ':completion:*' completer _extensions _complete _approximate
-zstyle ':completion:*' use-cache on
-zstyle ':completion:*' cache-path "$HOME/.zcompcache"
-zstyle ':completion:*' menu select
-zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
-zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
